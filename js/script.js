831323b4c0f59f3f200ce4cc5b1852371c7883ab
sahildhingra221 <sahildhingra221@gmail.com>
2018-07-18 23:20:15 +0530

    --
    --

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var pContainerHeight = $('.bird-box').height();
+
+$(window).scroll(function(){
+
+  var wScroll = $(this).scrollTop();
+
+  if (wScroll <= pContainerHeight) {
+
+    $('.logo').css({
+      'transform' : 'translate(0px, '+ wScroll /2 +'%)'
+    });
+
+    $('.back-bird').css({
+      'transform' : 'translate(0px, '+ wScroll /4 +'%)'
+    });
+
+    $('.fore-bird').css({
+      'transform' : 'translate(0px, -'+ wScroll /40 +'%)'
+    });
+
+  }
+  
+});
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("navBar");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
